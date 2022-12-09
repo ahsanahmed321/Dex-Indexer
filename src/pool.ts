73eecff36e6bb3e0e6eb6c0dcf9655fc504f75ec
ahsanahmed321 <88315014+ahsanahmed456@users.noreply.github.com>
2022-12-09 17:43:47 +0500

    initial commit

--- src/pool.ts
+++ src/pool.ts
@@ -1,3 +1,11 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import { Pool } from "../generated/Pool/Pool"
-import { ExampleEntity } from "../generated/schema"
+import { Liquidity } from "../generated/schema"
+
+export function handleAddLiquidity(callData:any) :void {
+const liquidity = new Liquidity (callData.params.id)
+liquidity.tokenA = callData.params.tokenAL
+liquidity.tokenB = callData.params.tokenBL
+liquidity.amountA = callData.params.amountTokenA
+liquidity.amountA = callData.params.amountTokenB
+}
